76e7e1c15663c1168c6f58ca5aa884ca93678f6f
deduzzo <Ninopalla9238222x2>
2018-11-08 18:40:48 +0100

    ok redundance and high availability

--- marginalex151.js
+++ marginalex151.js
@@ -1,10 +1,19 @@
 var config = {
+    payout: { value: 3, type: 'multiplier', label: 'payout' },
     baseBet: { value: 1000, type: 'balance', label: 'base bet' },
-    maxBets: { value: 500000, type: 'balance', label: 'max bets' }
+    mult: { value: 1.5, type: 'multiplier', label: 'mult after ko' },
+    strategy: {
+        value: 'times', type: 'radio', label: 'Strategy:',
+        options: {
+            times: { value: '11', type: 'multiplier', label: 'times' },
+            maxBets: { value: '1000000', type: 'balance', label: 'value' },
+        }
+    },
+    lateTimes: { value: 0, type: 'multiplier', label: 'times to late' },
 };
 
-const payout = 3;
-const increaseMult = 1.5;
+const payout = config.payout.value;
+const increaseMult = config.mult.value;
 let disaster = 0;
 let currentRound = 0;
 
@@ -12,17 +21,26 @@ log('Script is running..');
 
 let currentBet = config.baseBet.value;
 let maxBets = 0;
-const betLimit = config.maxBets.value;
+const strategy = config.strategy.value;
+const betLimit = config.strategy.options.maxBets.value;
+const maxTimes = config.strategy.options.times.value;
+const lateTimes = config.lateTimes.value;
+let maxTimesEver = 0;
+let currentTimes = 1;
+let timesToStart = lateTimes;
 
-// Always try to bet when script is started
-engine.bet(currentBet, payout);
+showStats(currentBet,increaseMult);
+
 
 engine.on('GAME_STARTING', onGameStarted);
 engine.on('GAME_ENDED', onGameEnded);
 
+
 function onGameStarted() {
-    log('ROUND , ', ++currentRound , ' - DIS: ', disaster, ' - betting', Math.round(currentBet / 100), 'on', payout, 'x');
-    engine.bet(currentBet, payout);
+    if (timesToStart==0) {
+        log('ROUND ', ++currentRound, ' - DIS: ', disaster, ' - betting', Math.round(currentBet / 100), 'on', payout, 'x');
+        engine.bet(currentBet, payout);
+    }
 }
 
 function onGameEnded() {
@@ -30,24 +48,65 @@ function onGameEnded() {
 
     // If we wagered, it means we played
     if (!lastGame.wager) {
-        return;
+        if (lateTimes > 0 || lastGame.bust >= payout) {
+            if (lastGame.bust >= payout) {
+                timesToStart = lateTimes;
+                log('bust ', lastGame.bust, ' resetting late time, wait for other ', timesToStart)
+            }
+            else if (timesToStart > 0) {
+                timesToStart--;
+                log('bust ', lastGame.bust, timesToStart == 0 ? ' ready to play!' : (' wait for other ' + timesToStart))
+            }
+        }
     }
-
-    // we won..
-    if (lastGame.cashedAt) {
-        currentBet = config.baseBet.value;
-        log('We won, so next bet will be', currentBet/100, 'bits')
-    } else {
-            currentBet = Math.round((currentBet /100) * increaseMult) * 100;
-        if (currentBet > betLimit) {
-            log('Was about to bet', currentBet, '> betlimit ', betLimit /100,', so restart.. :(');
-            disaster++;
+    else if (timesToStart==0) {
+        // we won..
+        if (lastGame.cashedAt) {
             currentBet = config.baseBet.value;
+            currentTimes = 1;
+            log('We won, so next bet will be', currentBet / 100, 'bits')
+            if (lateTimes >0) timesToStart = lateTimes;
+        } else {
+            currentBet = Math.round((currentBet / 100) * increaseMult) * 100;
+
+            if (strategy == 'maxBets' && currentBet > betLimit) {
+                log('Was about to bet', currentBet, '> betlimit ', betLimit / 100, ', so restart.. :(');
+                disaster++;
+                currentTimes = 1;
+                currentBet = config.baseBet.value;
+                if (lateTimes >0) timesToStart = lateTimes;
+            }
+            else if (strategy == 'times' && currentTimes > maxTimes) {
+                log('Was about to bet', currentTimes, '> max bet times, so restart.. :(');
+                disaster++;
+                currentTimes = 1;
+                currentBet = config.baseBet.value;
+                if (lateTimes >0) timesToStart = lateTimes;
+            }
+            else {
+                currentTimes++;
+                if (currentBet > maxBets) {
+                    maxBets = currentBet;
+                }
+                if (currentTimes > maxTimes)
+                    maxTimes = currentTimes;
+            }
+            log('LOST, so', currentBet / 100, 'bits, maxbets = ', maxBets / 100, '- T:', currentTimes, strategy == 'times' ? (' - MAXT:' + maxTimes) : ('MAXBET:' + betLimit / 100))
         }
-        else if (currentBet > maxBets) {
-            maxBets = currentBet;
-        }
-        log('We lost, so next bet will be', currentBet / 100, 'bits, maxbets = ', maxBets / 100)
     }
 
+}
+
+function showStats(initBet, mult)
+{
+    let i;
+    let count = 0;
+    let bet = initBet;
+    log("------ INFO -----")
+    for (i =1; i<30; i++)
+    {
+        count+=bet;
+        log('T:',i,' - bet:', (bet /100).toLocaleString('de-DE'), ' - tot: ', (count /100) .toLocaleString('de-DE'));
+        bet = Math.round((bet /100) * mult) * 100;
+    }
 }
