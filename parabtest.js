02851e799b576f6cf5df771f52ac278e9a0052cc
deduzzo <Ninopalla9238222x2>
2019-03-30 13:53:19 +0100

    ok redundance and high availability

--- parabtest.js
+++ parabtest.js
@@ -3,61 +3,57 @@ var config = {
         value: 100,
         type: 'balance'
     },
-    gainFactor: {
-        value: 6,
-        type: 'multiplier',
-        label: 'gain factor'
-    },
-    basePayout: {
-        value: 2,
-        type: 'multiplier',
-        label: 'base payout'
-    },
-    late: {
-        value: 2,
-        type: 'multiplier',
-        label: 'late'
-    },
+    mult: { value: 10, type: 'multiplier', label: 'Max Mult'},
 };
 
 
 log('Script is running..');
 
-var currentBaseBet = config.bet.value;
-var basePayout = config.basePayout.value;
-var totbet = 0;
-var n = 0;
 
 // Always try to bet when script is started
-engine.bet(currentBaseBet, basePayout);
+engine.bet(1, 1.01);
 
 engine.on('GAME_STARTING', onGameStarted);
 engine.on('GAME_ENDED', onGameEnded);
 
+calculateBets(config.mult.value, config.bet.value,Math.round(config.mult.value * 12.22));
+
+
 function onGameStarted() {
-    log('betting', Math.round(currentBaseBet / 100), 'at payout of', basePayout, 'x')
-    engine.bet(currentBaseBet, basePayout);
+
 }
 
 function onGameEnded(info) {
-    var lastGame = engine.history.first()
-    totbet +=lastGame.wager;
-    n++;
-    // If we wagered, it means we played
-    if (!lastGame.wager) {
-        return;
-    }
 
-    // we won..
-    if (lastGame.cashedAt) {
-        totbet = 0;
-        n=0;
-        currentBaseBet = config.bet.value;
-        basePayout = config.basePayout.value;
-    } else {
-        currentBaseBet *= 1 + Math.pow(2, -8.4 + (n / 100));
-        log(1 + Math.pow(2, -8.4 - (n / 1000)));
-        basePayout = (totbet / currentBaseBet) + config.gainFactor.value;
-    }
+}
 
+function calculateBets(mult,initBet,desideredT)
+{
+    let i;
+    let gain = (mult * initBet) - mult;
+    let amount = initBet;
+    let lastBet = 0;
+    let nextBet = 0;
+    log("T:", 0, " - bet: ", printBit(initBet, 0), " - TOT:", printBit(initBet, 1));
+    for (i=1; i<desideredT; i++)
+    {
+        lastBet = nextBet;
+        amount += lastBet;
+        nextBet = roundBit((amount + gain) / mult);
+        log("T:", i, " - bet: ", printBit(nextBet, 0), " - TOT:", printBit(amount, 1));
+    }
+}
+
+function roundBit(bet) {
+    return Math.round(bet / 100) * 100;
+}
+
+
+function printBit(bet, fixed) {
+    let suffix = "";
+    if (bet > 10000000 && bet <100000000)
+        suffix = "k";
+    else if (bet >100000000)
+        suffix = "M";
+    return (bet / (suffix == "" ? 100 : (suffix == "k" ? 100000 : (suffix == "M" ? 100000000 : "")))).toFixed(fixed).toLocaleString('de-DE') + suffix;
 }
