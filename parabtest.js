faf97d937a5903d3599213f83d80b44a6ea65a1b
deduzzo <Ninopalla9238222x2>
2019-03-31 01:40:02 +0100

    ok check integrity and sorting bug. Console colorful

--- parabtest.js
+++ parabtest.js
@@ -8,10 +8,11 @@ var config = {
 
 log('Script is running..');
 
-const min = 111;
-const max = 1599;
-let currentx = (Math.floor(Math.random() * (max - min)) + min) / 100;
-let progression = calculateBets(currentx, config.bet.value,Math.round(currentx * 13), false);
+const values=[2.12, 2.78, 3.5, 4.32, 5.23, 6.82, 7.54, 8.12, 9.1, 10.23, 11.32, 12.34, 13.51, 14.82, 15.3, 15.92];
+const progressions = []
+let currentxIndex = 0;
+for (var n =0; n< values.length; n++)
+    progressions[n] = calculateBets(values[n], config.bet.value,Math.round(values[currentxIndex] * 13), false);
 let i = 0;
 
 
@@ -21,8 +22,8 @@ engine.on('GAME_ENDED', onGameEnded);
 
 
 function onGameStarted() {
-    log ("T", i , " - Bet ", roundBit(progression[i]) / 100, " on", currentx);
-    engine.bet(roundBit(progression[i]), currentx);
+    log ("T", i , " - Bet ", roundBit(progressions[currentxIndex][i]) / 100, " on", values[currentxIndex]);
+    engine.bet(roundBit(progressions[currentxIndex][i]), values[currentxIndex]);
 }
 
 function onGameEnded(info) {
@@ -34,8 +35,7 @@ function onGameEnded(info) {
 
     // we won..
     if (lastGame.cashedAt) {
-        currentx = (Math.floor(Math.random() * (max - min)) + min) / 100;
-        progression = calculateBets(currentx, config.bet.value,Math.round(currentx * 13), false);
+        currentxIndex = (Math.floor(Math.random() * (values.length -1))) / 100;
         i = 0;
     } else {
         i++;
