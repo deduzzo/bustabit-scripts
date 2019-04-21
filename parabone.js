ea0625ad428286ef33c0fc42c4d6ba767df1a91c
deduzzo <Ninopalla9238222x2>
2019-04-21 02:51:29 +0200

    ok check integrity and sorting bug. Console colorful

--- parabone.js
+++ parabone.js
@@ -1,134 +1,64 @@
 var config = {
-    payout: { value: 1.2, type: 'multiplier', label: 'Mult' },
-    baseBet: { value: 1000, type: 'balance', label: 'Base Bet' },
-    mult: { value: 1.04, type: 'multiplier', label: 'x after KO' },
-    strategy: {
-        value: 'freeze', type: 'radio', label: 'Strategy:',
-        options: {
-            maxBets: { value: '10000,00', type: 'multiplier', label: 'Max Bet' },
-            freeze: { value: '60', type: 'multiplier', label: 'Last T before flat bet' },
-        }
-    },
-    maxTimes: { value: 8, type:'multiplier', label: 'Max Times'},
-    lateTimes: { value: 0, type: 'multiplier', label: 'Late by x times' },
-    disasterWaits: {value: 0, type:'multiplier', label: 'Disaster waits:'}
+    bet: { value: 1500, type: 'balance' },
+    payout: { value: 15, type: 'multiplier' },
 };
 
 
-let payout = config.payout.value;
-const increaseMult = config.mult.value;
-let disaster = 0;
-let currentRound = 0;
-
 log('Script is running..');
 
-let currentBet = config.baseBet.value;
-let sumdBet = 0;
-let maxBets = 0;
-const strategy = config.strategy.value;
-const betLimit = config.strategy.options.maxBets.value;
-const freezeFrom = config.strategy.options.freeze.value;
-const lateTimes = config.lateTimes.value;
-const maxTimes = config.maxTimes.value;
-const disasterWaits  = config.disasterWaits.value;
-let maxTimesEver = 0;
-let currentTimes = 0;
-let timesToStart = lateTimes;
-let disasterToStart = 0;
-let freezing = false;
 
-showStats(currentBet,increaseMult);
-
+// Always try to bet when script is started
+engine.bet(100, config.payout.value);
 
 engine.on('GAME_STARTING', onGameStarted);
 engine.on('GAME_ENDED', onGameEnded);
 
+let k = 0;
+const payout = config.payout.value;
+const bet = config.bet.value;
+let values = calculateBets(parseFloat(payout) , bet,parseFloat(payout) * 10, false);
 
 function onGameStarted() {
-    if (disasterToStart == 0) {
-        if (timesToStart == 0) {
-            log('ROUND ', ++currentRound, ' - DIS: ', disaster, ' - betting', Math.round(currentBet / 100), 'on', payout, 'x');
-            engine.bet(currentBet, payout);
-        }
-    }
-    else
-    {
-        log('DISASTER WAIT, ', disasterToStart--, ' games to start - DIS: ', disaster);
-    }
+        log(k, " bet ", roundBit(values[k]) / 100);
+        engine.bet(values[k], payout);
+        k++;
 }
 
 function onGameEnded() {
-    var lastGame = engine.history.first()
+    var lastGame = engine.history.first();
 
-    // If we wagered, it means we played
-    if (!lastGame.wager) {
-        if ((lateTimes > 0 || lastGame.bust >= payout) && !freezing) {
-            if (lastGame.bust >= payout) {
-                timesToStart = lateTimes;
-                log('bust ', lastGame.bust, ' resetting late time, wait for other ', timesToStart)
-            }
-            else if (timesToStart > 0) {
-                timesToStart--;
-                log('bust ', lastGame.bust, timesToStart == 0 ? ' ready to play!' : (' wait for other ' + timesToStart))
-            }
-        }
+    // we won..
+    if (lastGame.cashedAt) {
+        k=0;
     }
-    else if (timesToStart==0) {
-        // we won..
-        if (lastGame.cashedAt) {
-            currentBet = config.baseBet.value;
-            currentTimes = 0;
-            if (disasterWaits >0 && freezing) disasterToStart = disasterWaits;
-            freezing = false;
-            log('We won, so next bet will be', currentBet / 100, 'bits')
-            if (lateTimes >0) timesToStart = lateTimes;
-        } else if (maxTimes >0 && currentTimes >= maxTimes) {
-            log('Was about to bet', currentTimes, '> max bet times, so restart.. :(');
-            if (disasterWaits >0) disasterToStart = disasterWaits;
-            disaster++;
-            currentTimes = 0;
-            currentBet = config.baseBet.value;
-            if (lateTimes > 0) timesToStart = lateTimes;
-            }
-        else {
-            if (!(strategy == 'freeze' && currentTimes >= freezeFrom)) {
-                currentBet = Math.ceil((currentBet / 100) * increaseMult) * 100;
-
-            }
-            else
-                freezing = true;
-            if (strategy == 'maxBets' && currentBet >= betLimit) {
-                log('Was about to bet', currentBet, '> betlimit ', betLimit / 100, ', so restart.. :(');
-                disaster++;
-                freezing = false;
-                if (disasterWaits >0) disasterToStart = disasterWaits;
-                currentTimes = 0;
-                currentBet = config.baseBet.value;
-                if (lateTimes > 0) timesToStart = lateTimes;
-            } else {
-                currentTimes++;
-                if (currentBet > maxBets) {
-                    maxBets = currentBet;
-                }
-                if (currentTimes > maxTimesEver)
-                    maxTimesEver = currentTimes;
-            }
-        }
-        if (disasterToStart == 0)
-            log('LOST, so', currentBet / 100, 'bits, maxbets = ', maxBets / 100, '- T:', currentTimes, ' - MAXT:' + maxTimesEver , strategy == 'maxBets' ? (' MAXBET: ' + betLimit / 100) : (strategy == 'freeze') ? ('FREEZE AT: ' + freezeFrom) : (''))
-        }
 }
 
-function showStats(initBet, mult)
+
+function calculateBets(mult,initBet,desideredT, verbose)
 {
-    let i;
-    let count = 0;
-    let bet = initBet;
-    log("------ INFO -----")
-    for (i =0; i<60; i++)
+    let progression = [];
+    progression[0] = initBet;
+    let k;
+    let gain = mult > 1.12 ? (mult * initBet) - initBet : 10000;
+    let amount = 0;
+    let lastBet = initBet;
+    for (k=0; k<desideredT; k++)
     {
-        count+=bet;
-        log('T:',i,' - bet:', (bet /100).toLocaleString('de-DE'), ' - tot: ', (count /100) .toLocaleString('de-DE'));
-        bet = Math.ceil((bet /100) * mult) * 100;
+        amount += lastBet;
+        if (verbose) log("T:", k, " - bet: ", printBit(lastBet), " - TOT:", printBit(amount), "G:",printBit((lastBet * mult) - amount));
+        let tempBet = (amount) / mult;
+        do {
+            if (mult <2)
+                tempBet += 10000;
+            else
+                tempBet+= 100;
+        } while (((tempBet * mult) - tempBet - amount) <= gain)
+        lastBet = roundBit(tempBet);
+        progression[k+1] = lastBet;
     }
+    return progression;
+}
+
+function roundBit(bet) {
+    return Math.round(bet / 100) * 100;
 }
