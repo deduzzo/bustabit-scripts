177c16b0ef4c6894f2022cc4392dd0b51ae63a5e
deduzzo <Ninopalla9238222x2>
2019-05-04 13:25:18 +0200

    ok redundance and high availability

--- marginale2x3.js
+++ marginale2x3.js
@@ -1,5 +1,6 @@
 var config = {
     payout: { value: 3, type: 'multiplier', label: 'Mult' },
+    payout2: { value: 1.8, type: 'multiplier', label: 'Mult 2' },
     baseBet: { value: 1000, type: 'balance', label: 'Base Bet' },
     mult: { value: 1.5, type: 'multiplier', label: 'x after KO' },
     start2After: { value: 15, type:'multiplier', label: 'Start 2 after'},
@@ -29,7 +30,7 @@ engine.on('GAME_ENDED', onGameEnded);
 function onGameStarted() {
     if (currentRound > config.start2After.value) {
         currentBet = calcBaseBet(roundBets);
-        payout = currentRound % 2 == 0 ? 2 : 3;
+        payout = currentRound % 3 == 0 ? config.payout.value : config.payout2.value
     }
     log('ROUND ', ++currentRound, ' - DIS: ', disaster, ' - betting', Math.round(currentBet / 100), 'on', payout, 'x');
     engine.bet(currentBet, payout);
@@ -39,13 +40,14 @@ function onGameEnded() {
     var lastGame = engine.history.first()
 
         // we won..
-        if (lastGame.cashedAt)
+        if (lastGame.cashedAt || roundBets<0 )
         {
-            if (currentRound < config.start2After.value || (currentRound > config.start2After.value && lastGame.cashedAt == 3)) {
+            if (roundBets<0 || lastGame.cashedAt >= 3) {
                 currentBet = config.baseBet.value;
                 payout = config.payout.value;
                 currentTimes = 0;
                 roundBets = 0;
+                currentRound = 0;
                 log('We won, so next bet will be', currentBet / 100, 'bits')
             }
             else {
@@ -83,5 +85,5 @@ function showStats(initBet, mult)
 
 function calcBaseBet(amount)
 {
-    return (amount / 3) + config.baseBet.value;
+    return (amount / 2) + config.baseBet.value;
 }
