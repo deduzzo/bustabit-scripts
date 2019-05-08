c34b6ee87b3a849830bf5c1c43b4c24d20af0fc5
deduzzo <Ninopalla9238222x2>
2019-05-08 19:23:31 +0200

    ok redundance and high availability

--- multiparab.js
+++ multiparab.js
@@ -2,63 +2,84 @@ var config = {
     p1: { value: 1.16, type: 'multiplier', label: '===> P1' },
     r1: { value: 6, type: 'multiplier', label: 'Late 1' },
     b1: { value: 9900000, type: 'balance', label: 'Bet 1'},
+    l1: { value: 7, type: 'multiplier', label: 'Max 1 (for Auto)'},
     p2: { value: 1.5, type: 'multiplier', label: '===> P2' },
     r2: { value: 11, type: 'multiplier', label: 'Late 2' },
     b2: { value: 9900000, type: 'balance', label: 'Bet 2'},
+    l2: { value: 12, type: 'multiplier', label: 'Max 2 (for Auto)'},
     p3: { value: 2, type: 'multiplier', label: '===> P3' },
     r3: { value: 11, type: 'multiplier', label: 'Late 3' },
     b3: { value: 30000, type: 'balance', label: 'Bet 3'},
+    l3: { value: 18, type: 'multiplier', label: 'Max 3 (for Auto)'},
     p4: { value: 3, type: 'multiplier', label: '===> P4' },
     r4: { value: 20, type: 'multiplier', label: 'Late 4' },
     b4: { value: 12000, type: 'balance', label: 'Bet 4'},
+    l4: { value: 34, type: 'multiplier', label: 'Max 4 (for Auto)'},
     p5: { value: 5, type: 'multiplier', label: '===> P5' },
     r5: { value: 34, type: 'multiplier', label: 'Late 5' },
     b5: { value: 10000, type: 'balance', label: 'Bet 5'},
+    l5: { value: 56, type: 'multiplier', label: 'Max 5 (for Auto)'},
     p6: { value: 10, type: 'multiplier', label: '===> P6' },
     r6: { value: 66, type: 'multiplier', label: 'Late 6' },
     b6: { value: 10000, type: 'balance', label: 'Bet 6'},
+    l6: { value: 105, type: 'multiplier', label: 'Max 6 (for Auto)'},
     p7: { value: 20, type: 'multiplier', label: '===> P7' },
     r7: { value: 166, type: 'multiplier', label: 'Late 7' },
     b7: { value: 10000, type: 'balance', label: 'Bet 7'},
+    l7: { value: 232, type: 'multiplier', label: 'Max 7 (for Auto)'},
     p8: { value: 40, type: 'multiplier', label: '===> P8' },
     r8: { value: 300, type: 'multiplier', label: 'Late 8' },
     b8: { value: 4000, type: 'balance', label: 'Bet 8'},
+    l8: { value: 440, type: 'multiplier', label: 'Max 8 (for Auto)'},
     p9: { value: 50, type: 'multiplier', label: '===> P9' },
     r9: { value: 351, type: 'multiplier', label: 'Late 9' },
     b9: { value: 4000, type: 'balance', label: 'Bet 9'},
+    l9: { value: 516, type: 'multiplier', label: 'Max 9 (for Auto)'},
     p10: { value: 100, type: 'multiplier', label: '===> P10' },
     r10: { value: 800, type: 'multiplier', label: 'Late 10' },
     b10: { value: 2000, type: 'balance', label: 'Bet 10'},
+    l10: { value: 1118, type: 'multiplier', label: 'Max 10 (for Auto)'},
     p11: { value: 200, type: 'multiplier', label: '===> P11' },
     r11: { value: 1400, type: 'multiplier', label: 'Late 11' },
     b11: { value: 1200, type: 'balance', label: 'Bet 11'},
+    l11: { value: 2019, type: 'multiplier', label: 'Max 11 (for Auto)'},
     p12: { value: 300, type: 'multiplier', label: '===> P12' },
     r12: { value: 1850, type: 'multiplier', label: 'Late 12' },
     b12: { value: 1200, type: 'balance', label: 'Bet 12'},
+    l12: { value: 2642, type: 'multiplier', label: 'Max 12 (for Auto)'},
     p13: { value: 500, type: 'multiplier', label: '===> P13' },
     r13: { value: 2450, type: 'multiplier', label: 'Late 13' },
     b13: { value: 1000, type: 'balance', label: 'Bet 13'},
+    l13: { value: 3634, type: 'multiplier', label: 'Max 13 (for Auto)'},
     p14: { value: 600, type: 'multiplier', label: '===> P14' },
     r14: { value: 3200, type: 'multiplier', label: 'Late 14' },
     b14: { value: 1000, type: 'balance', label: 'Bet 14'},
+    l14: { value: 4530, type: 'multiplier', label: 'Max 14 (for Auto)'},
     p15: { value: 800, type: 'multiplier', label: '===> P15' },
     r15: { value: 3900, type: 'multiplier', label: 'Late 15' },
     b15: { value: 1000, type: 'balance', label: 'Bet 15'},
+    l15: { value: 5527, type: 'multiplier', label: 'Max 15 (for Auto)'},
     p16: { value: 1000, type: 'multiplier', label: '===> P16' },
     r16: { value: 4400, type: 'multiplier', label: 'Late 16' },
     b16: { value: 1000, type: 'balance', label: 'Bet 16'},
+    l16: { value: 6168, type: 'multiplier', label: 'Max 16 (for Auto)'},
     p17: { value: 2000, type: 'multiplier', label: '===> P17' },
     r17: { value: 10000, type: 'multiplier', label: 'Late 17' },
     b17: { value: 500, type: 'balance', label: 'Bet 17'},
+    l17: { value: 13476, type: 'multiplier', label: 'Max 17 (for Auto)'},
     p18: { value: 4000, type: 'multiplier', label: '===> P18' },
     r18: { value: 17000, type: 'multiplier', label: 'Late 18' },
     b18: { value: 400, type: 'balance', label: 'Bet 18'},
+    l18: { value: 22196, type: 'multiplier', label: 'Max 18 (for Auto)'},
     p19: { value: 7000, type: 'multiplier', label: '===> P19' },
     r19: { value: 30000, type: 'multiplier', label: 'Late 19' },
     b19: { value: 200, type: 'balance', label: 'Bet 19'},
+    l19: { value: 40756, type: 'multiplier', label: 'Max 19 (for Auto)'},
     p20: { value: 10000, type: 'multiplier', label: '===> P20' },
     r20: { value: 28000, type: 'multiplier', label: 'Late 20' },
     b20: { value: 100, type: 'balance', label: 'Bet 20'},
+    l20: { value: 45885, type: 'multiplier', label: 'Max 20 (for Auto)'},
+    autoValue: { value: 1, type: 'multiplier', label: '1 for Auto'},
     multx: { value: 1, type: 'multiplier', label: 'Mult All Bet for x'},
     low: { value: 0, type: 'multiplier', label: '0: High Before, 1 Low Before'},
     balance: { value: 10000000, type: 'balance', label: 'Balance to use'},
@@ -77,19 +98,25 @@ let latesValue = [];
 let playedGames = [];
 let k =0;
 let multFactor = 1;
+let gains = [];
 
 for (let i =1; i<21; i++) {
     let st = "p" + i.toString();
     if (!isNaN(config[st].value) && config[st].value != null)
         values[(config[st].value).toString()] = calculateBets(config[st].value, 100, config[st].value * 10, false);
+    if (config.autoValue.value == 1)
+        config["b" + i.toString()].value = calculateAutoBet(config.balance.value, config["r" + i.toString()].value, config["l" + i.toString()].value, config["p" + i.toString()].value)
 }
 
 for (let key of Object.keys(values)) {
-        latesValue[key] = 0;
-        playedGames[key] = 0;
+    latesValue[key] = 0;
+    playedGames[key] = 0;
+    gains[key] = 0;
 }
 
-showMax()
+showMax();
+
+    log("bet1.16:", calculateAutoBet(config.balance.value, 2, 7, values["1.16"]));
 
 function onGameStarted() {
     if (currentValue.value != -1) {
@@ -107,6 +134,7 @@ function onGameEnded() {
         log("Bust:", lastGame.bust);
         if (lastGame.cashedAt)
         {
+            gains[currentValue.value] = gains[currentValue.value] + ((lastGame.cashedAt * lastGame.wager) - lastGame.wager);
             currentValue = {value: -1, lates: 0}
             log(lastGame.bust, " WIN!!");
             showGames();
@@ -140,7 +168,7 @@ function calculateBets(mult,initBet,desideredT, verbose)
         let tempBet = (amount) / mult;
         do {
             if (mult <2)
-                tempBet += 10000;
+                tempBet += 1000;
             else
                 tempBet+= 100;
         } while (((tempBet * mult) - tempBet - amount) <= gain)
@@ -169,6 +197,15 @@ function updateLates(lastBust, lv)
     return lv;
 }
 
+function printBit(bet) {
+    let suffix = "";
+    if (bet > 10000000 && bet <100000000)
+        suffix = "k";
+    else if (bet >100000000)
+        suffix = "M";
+    return (bet / (suffix == "" ? 100 : (suffix == "k" ? 100000 : (suffix == "M" ? 100000000 : "")))).toLocaleString('de-DE') + suffix;
+}
+
 function findFirstLateValue(l)
 {
     if(config.low.value)
@@ -193,13 +230,11 @@ function findFirstLateValue(l)
 function showGames()
 {
     log("STATS:");
-    let str = "";
     for (let i=1; i<21; i++)
     {
         if (!isNaN(config["p"+ i.toString()].value) && config["p"+ i.toString()].value != null)
-            str += config["p"+ i.toString()].value + ": " + playedGames[(config["p"+ i.toString()].value).toString()] +" - ";
+            log(config["p"+ i.toString()].value + ": " + playedGames[(config["p"+ i.toString()].value).toString()] +" game -> ", printBit(gains[config["p"+ i.toString()].value]));
     }
-    log (str.substr(0,str.length -2));
 }
 
 function showMax()
@@ -214,10 +249,27 @@ function showMax()
                 amount += roundBit(values[(config["p" + i.toString()].value).toString()][k++] * (config["b" + i.toString()].value / 100) * config.multx.value);
             }
             k--;
-            log(config["p" + i.toString()].value, " ->", config["r" + i.toString()].value, " + ", k, " = T", config["r" + i.toString()].value + k);
+            log(config["p" + i.toString()].value, " ->", config["r" + i.toString()].value, " + ", k, " = T", config["r" + i.toString()].value + k, " bet ", config.autoValue.value == 1 ? "[A] ": " ", config["b" + i.toString()].value);
             amount = 0;
             k = 0;
         }
     }
+
+}
+
+function calculateAutoBet(amount, late, maxT, values)
+{
+    let tot =0;
+    let bet = 0;
+
+    do
+    {
+        bet += 100;
+        for (let i =0; i<(maxT - late); i++)
+        {
+            tot += values[i];
+        }
+    } while (tot<amount)
+    return (bet - 100) / 100;
 
 }
